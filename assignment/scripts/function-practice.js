a71c41a1524302cd255936bab33ff7c02973b4da
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-09 13:59:14 -0500

    problem 6 completed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -70,15 +70,32 @@ console.log(isPositive(-3));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
 
+
+
+function getLast( array ) {
+   console.log('array items =' ,  array.length)
+  if ( array.length > 0){
+    
+    return array[array.length -1];
+  
+  }else 
+  
+  return '0 items returns undefined';
+  
 }
+
+console.log(getLast([]));
+console.log(getLast([1, 5, 11]));
+
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
   
+
 }
 
 // ----------------------
